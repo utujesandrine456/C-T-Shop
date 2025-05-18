2ead878bfb60d9bb4540456323f35c65786e10fe
utujesandrine456 <utujesandrine456@gmail.com>
2025-05-18 16:22:40 +0200

    Coffee project is available for all people who love and also like coffee in their daily life

--- coffee.js
+++ coffee.js
@@ -1,19 +1,18 @@
-// Prevent default anchor link behavior
+
 document.querySelectorAll('.slider-nav a').forEach((navLink) => {
-    navLink.addEventListener('click', (event) => {
-        event.preventDefault(); // Prevent default anchor link behaviour.
-        const targetSlide = document.querySelector(navLink.getAttribute('href')); //Get the target slide.
-        targetSlide.scrollIntoView({ behavior: 'smooth', block: 'nearest' }); // Scroll to the slide smoothly, keeping it near the top.
+    navLink.addEventListener('click', () => {
+        const targetSlide = document.querySelector(navLink.getAttribute('href')); 
+        targetSlide.scrollIntoView({ behavior: 'smooth', block: 'nearest' }); 
     });
 }); 
 
-// To allow the form in .main class to be displayed.
+
 document.getElementById('register-btn').addEventListener('click', () => {
     const mainContainer = document.querySelector('.main');
     mainContainer.style.display = mainContainer.style.display === 'block' ? 'none' : 'block';
 });
 
-// to highlight a link when scrooled to or clicked on.
+
 document.addEventListener('DOMContentLoaded', () => {
 const sections = document.querySelectorAll('.section');
 const navLinks = document.querySelectorAll('.nav-link'); 
@@ -24,12 +23,40 @@ const updateActiveLink = () => {
     const sectionBottom = sectionTop + section.offsetHeight;
 
 if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
-    navLinks.forEach((link) => link.classList.remove('active')); // Remove 'active' from all links
-    navLinks[index].classList.add('active'); // Add 'active' to the link matching the section
+    navLinks.forEach((link) => link.classList.remove('active')); 
+    navLinks[index].classList.add('active'); 
     }
     });
 };
 
-// Update active link on scroll
+
 window.addEventListener('scroll', updateActiveLink);
+});
+
+
+//Slides JS codes
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item'); 
+    document.querySelector('.slide').appendChild(items[0]); 
+});
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item'); 
+    document.querySelector('.slide').prepend(items[items.length - 1]); 
+});
+
+
+window.addEventListener("message", function(event) {
+    if (event.data === "redirectToHome") {
+        window.location.href = "http://127.0.0.1:5500/CRUD-2025/public/Food-API/index.html";
+    }
+});
+
+document.querySelectorAll(".fa-xmark").forEach(icon => {
+    icon.addEventListener("click", () =>{
+        icon.parentElement.remove();
+    });
 });
